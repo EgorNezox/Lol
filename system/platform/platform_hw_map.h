39978a0294b687710eac27981399b429ef41c482
Artem Pisarenko <05-15@oniip.local>
2015-10-28 10:59:47 +0600

    Added hardware platform resources definitions. refs #824
    Signed-off-by: Artem Pisarenko <05-15@oniip.local>

--- system/platform/platform_hw_map.h
+++ system/platform/platform_hw_map.h
@@ -2,7 +2,7 @@
   ******************************************************************************
   * @file    platform_hw_map.h
   * @author  Artem Pisarenko, PMR dept. software team, ONIIP, PJSC
-  * @date    23.10.2015
+  * @date    26.10.2015
   * @brief   Определения назначения аппаратных ресурсов платформы
   *
   ******************************************************************************
@@ -11,13 +11,21 @@
 #ifndef PLATFORM_HW_MAP_H_
 #define PLATFORM_HW_MAP_H_
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+typedef enum {
+	platformhwInvalidResource = 0,
+	platformhwHeadsetUart,				// UART RS-232 гарнитуры
+	platformhwHeadsetPttIopin,			// I/O-пин тангенты гарнитуры
+	platformhwDataFlashSpi,				// SPI доступа Data-Flash
+	platformhwMatrixKeyboard,			// матричная клавиатура
+	platformhwKeyboardButt1Iopin,		// I/O-пин кнопки BUTT_1 двухкнопочной клавиатуры
+	platformhwKeyboardButt2Iopin,		// I/O-пин кнопки BUTT_2 двухкнопочной клавиатуры
+	platformhwKeyboardsLightIopin,		// I/O-пин управления подсветкой клавиатур
+	platformhwEnRxRs232Iopin,			// I/O-пин входа EN_RX_232 трансивера последовательного порта
+	platformhwEnTxRs232Iopin,			// I/O-пин входа EN_TX_232 трансивера последовательного порта
+	platformhwDspUart,					// UART DSP
+	platformhwDspResetIopin,			// I/O-пин сигнала RESET DSP
+	platformhwAtuUart,					// UART АСУ
+	platformhwBatterySmbusI2c			// I2C шины SMBus аккумуляторной батареи
+} platformhw_resource_t;
 
-
-
-#ifdef __cplusplus
-}
-#endif
 #endif /* PLATFORM_HW_MAP_H_ */
