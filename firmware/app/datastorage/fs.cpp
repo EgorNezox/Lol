e42dd6b068e1261b91bdeb598b404186e71825ec
Artem Pisarenko <05-15@oniip.local>
2015-12-25 16:09:53 +0600

    Установлена фиксированная таблица речевых каналов (14 открытых каналов от Мартынова А.В.)

--- firmware/app/datastorage/fs.cpp
+++ firmware/app/datastorage/fs.cpp
@@ -27,9 +27,40 @@ void FS::init() {
 }
 
 bool FS::getVoiceChannelsTable(Multiradio::voice_channels_table_t& data) {
-	QM_UNUSED(data);
-	//...
-	return false;
+	Multiradio::voice_channel_entry_t ch_entry;
+	ch_entry.type = Multiradio::channelOpen;
+	ch_entry.speed = Multiradio::voicespeedInvalid;
+
+	ch_entry.frequency = 3000000;
+	data.push_back(ch_entry);
+	ch_entry.frequency = 5000000;
+	data.push_back(ch_entry);
+	ch_entry.frequency = 8000000;
+	data.push_back(ch_entry);
+	ch_entry.frequency = 10000000;
+	data.push_back(ch_entry);
+	ch_entry.frequency = 15000000;
+	data.push_back(ch_entry);
+	ch_entry.frequency = 17000000;
+	data.push_back(ch_entry);
+	ch_entry.frequency = 27000000;
+	data.push_back(ch_entry);
+	ch_entry.frequency = 30000000;
+	data.push_back(ch_entry);
+	ch_entry.frequency = 31000000;
+	data.push_back(ch_entry);
+	ch_entry.frequency = 35000000;
+	data.push_back(ch_entry);
+	ch_entry.frequency = 40000000;
+	data.push_back(ch_entry);
+	ch_entry.frequency = 45000000;
+	data.push_back(ch_entry);
+	ch_entry.frequency = 48000000;
+	data.push_back(ch_entry);
+	ch_entry.frequency = 50000000;
+	data.push_back(ch_entry);
+
+	return true;
 }
 
 } /* namespace DataStorage */
