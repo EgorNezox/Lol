75c430b8951117783be1d80e18079d2393a82c70
Artem Pisarenko <05-15@oniip.local>
2016-01-14 14:47:53 +0600

    добавил костыль: при сбросе осуществляется установка gpio-пинов, предотвращающих спонтанную активацию spi-интерфейса sky72310 и преждевременный запуск DSP

--- firmware/system_stm32f2/hardware_startup.c
+++ firmware/system_stm32f2/hardware_startup.c
@@ -9,8 +9,19 @@
   */
 
 #include <stdint.h>
+#include "stm32f2xx.h"
 #include "system_stm32f2xx.h"
 #include "system_hw_io.h"
+
+static void preinit_hardware_io(void) {
+	/* Set CS_PLL pin (PH15) to high level (prevent spurious activation of sky72310 spi interface) */
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOHEN;
+	GPIOH->MODER |= GPIO_MODER_MODER15_0;
+	GPIOH->ODR |= GPIO_ODR_ODR_15;
+	/* Set RESET_DSP pin (PH13) to low level (prevent DSP from early startup) */
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOHEN;
+	GPIOH->MODER |= GPIO_MODER_MODER13_0;
+}
 
 /*! Обработчик системного сброса (точка входа в загрузчик)
  *
@@ -18,6 +29,7 @@
  * Внимание: память на данном этапе не инициализирована, в данном контексте нельзя обращаться к глобальным переменным !
  */
 void Reset_Handler(void) {
+	preinit_hardware_io();
 	SystemInit(); // перемещено из System_Startup_Entry
 	stm32f2_ext_mem_init();
 	if (!stm32f2_ext_sram_test())
