3cc121b8232986c94889ee20ab0032e45bf9e85f
Pankov Denis <markaizek@gmail.com>
2016-04-01 11:47:14 +0600

    add time and data

--- firmware/app/ui/service.cpp
+++ firmware/app/ui/service.cpp
@@ -743,6 +743,8 @@ void Service::setCoordDate(Navigation::Coord_Date*(*func)())
    Navigation::Coord_Date* date = (Navigation::Coord_Date*)func;
    menu->coord_lat.append((char *)date->latitude);
    menu->coord_log.append((char *)date->longitude);
+   menu->date.append((char *)date->data);
+   menu->time.append((char *)date->time);
 }
 
 
