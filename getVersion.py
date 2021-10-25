69d9188a69279d27549fddbda87cc9e2e313727f
Alexandr Kondratyev <drunkOwl@nodomain.com>
2021-10-25 09:59:59 +0600

    to archive: slow down fsmc

--- getVersion.py
+++ getVersion.py
@@ -6,25 +6,27 @@ git = "C:\Program Files\Git\git-cmd.exe"
 cmd = "git log -1 --oneline"
 
 def get_version():
-    process = subprocess.check_output([git, cmd])
-    lines = process.decode('utf-8').split()
-    return lines[0]
+	process = subprocess.check_output([git, cmd])
+	lines = process.decode('utf-8').split()
+	return lines[0]
 
 def add_version_to_file():
-    new_line = "#define HOST_VERSION " + '"' + get_version() + '"'  + "\n"
-    file_name = "sazhenn.h"
+	version = get_version()[:7]
+	print("Build version: ", version)
+	new_line = "#define HOST_VERSION " + '"' + version + '"'  + "\n"
+	file_name = "sazhenn.h"
 	
-    with open(file_name, 'r') as file:
-        data = file.readlines()
+	with open(file_name, 'r') as file:
+		data = file.readlines()
 
-    data[14] = new_line
+	data[14] = new_line
 
-    with open(file_name, 'w') as file:
-        file.writelines( data )
+	with open(file_name, 'w') as file:
+		file.writelines( data )
 
 def main():
 	add_version_to_file()
 
 if __name__ == "__main__":
-    # execute only if run as a script
-    main()
+	# execute only if run as a script
+	main()
